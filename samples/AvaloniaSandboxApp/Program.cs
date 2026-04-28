fde4f1d12b918d09b2d93759bebcc64c46686b37
cedri <cedric.boutin56@gmail.com>
2026-04-28 17:00:14 +0200

    Removed UseAvaloniaEssentials

--- samples/AvaloniaSandboxApp/Program.cs
+++ samples/AvaloniaSandboxApp/Program.cs
@@ -16,7 +16,6 @@ class Program
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
-            .UseAvaloniaEssentials()
             .WithInterFont()
             .LogToTrace();
 }
